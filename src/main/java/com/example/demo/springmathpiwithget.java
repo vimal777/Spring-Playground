8da7e27cddfb08857c6c5d043d9380808e2b9359
vimal777 <vimal.sawant@capgemini.com>
2017-07-19 17:43:00 +0530

    Update springmathpiwithget.java
    app removed

--- src/main/java/com/example/demo/springmathpiwithget.java
+++ src/main/java/com/example/demo/springmathpiwithget.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/app")
+@RequestMapping("/")
 
 public class springmathpiwithget {
     @GetMapping("/math/pi")
